6b6807713b5e50cadd88af90f0d9669e81fc7056
Coaxus <64023053+Coaxus-ux@users.noreply.github.com>
2022-06-06 17:27:20 -0500

    login is conect to back

--- src/view/Login.svelte
+++ src/view/Login.svelte
@@ -23,15 +23,14 @@
 </script>
 
 <div
-  class="grid   grid-cols-1 md:grid-cols-2 lg:grid-cols-3 grid-rows-1 grid-flow-row w-auto h-screen"
+  class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 grid-rows-1 grid-flow-row w-auto h-screen"
 >
   <LeftImage srcImage="../src/assets/logo_login.svg" />
-
   <div
-    class="bg-white flex flex-col mx-auto col-span-1 md:col-span-2  content-center  justify-center  w-9/12"
+    class="bg-white flex gap-10 flex-col mx-auto col-span-1 md:col-span-2 content-center justify-center md:w-5/12 w-9/12"
   >
     <div>
-      <p class="text-center text-4xl mb-36 font-bold text-black">
+      <p class="text-center text-5xl mb-5 font-bold text-black">
         Iniciar sesión
       </p>
     </div>
@@ -42,14 +41,13 @@
         Correo o Contraseña incorrectos
       </div>
     {/if}
-
     <form>
       <div
-        class="w-full flex flex-col  justify-center  focus-within:ring-blue-400 "
+        class="w-full flex flex-col justify-center gap-2 mb-5"
       >
         <label
           for="emailInput"
-          class="text-sm ml-2  font-bold text-black leading-5 "
+          class="text-md ml-2 font-semibold text-black leading-5"
         >
           Email
         </label>
@@ -58,16 +56,15 @@
           type="email"
           bind:value={$userData.email}
           placeholder="ejemplo@mail.com"
-          class="my-3 w-full bg-slate-50 h-10 p-2 rounded mt-0  border-slate-200 focus:outline-none"
+          class="focus:ring-blue-400 focus:ring-2 my-3 w-full bg-slate-50 h-10 p-2 rounded mt-0  border-slate-200 focus:outline-none"
         />
       </div>
-
       <div
-        class="w-full flex flex-col  justify-center  focus-within:ring-blue-400 "
+        class="w-full gap-2 flex flex-col justify-center"
       >
         <label
           for="passwordInput"
-          class="text-sm ml-2 font-bold text-black leading-5 "
+          class="text-md ml-2 font-semibold text-black leading-5"
         >
           Contaseña
         </label>
@@ -76,19 +73,18 @@
           type="password"
           bind:value={$userData.password}
           placeholder="Contraseña"
-          class="my-3 w-full bg-slate-50 h-10 p-2 rounded mt-0  border-slate-200 focus:outline-none"
+          class="focus:ring-blue-400 focus:ring-2 my-3 w-full bg-slate-50 h-10 p-2 rounded mt-0  border-slate-200 focus:outline-none"
         />
       </div>
     </form>
-
     <div class="flex items-start  mx-3">
-      <div class="flex items-start">
+      <div class="flex center">
         <div class="flex items-center">
           <input
             id="remember"
             aria-describedby="remember"
             type="checkbox"
-            class="bg-gray-300 border border-gray-400 focus:ring-3 focus:ring-emerald-500 h-4 w-4 rounded"
+            class="cursor-pointer bg-gray-300 border border-gray-400 focus:ring-3 focus:ring-emerald-500 h-4 w-4 rounded"
           />
         </div>
         <div class="text-sm ml-3">
@@ -99,22 +95,22 @@
       </div>
       <a
         href="/register"
-        class="text-sm text-blue-500 ml-auto hover:text-blue-6  00"
-        >Olvide mi Contraseña</a
+        class="text-sm text-blue-500 ml-auto hover:text-blue-600"
+        >Olvidé mi Contraseña</a
       >
     </div>
     <div
-      class="mt-8 flex flex-col mx-auto  content-center justify-center w-8/12 text-black"
+      class="flex flex-col mx-auto  content-center justify-center w-8/12 text-black mt-5"
     >
       <button
-        class="bg-blue-300 drop-shadow-md duration-500 hover:bg-blue-400 font-bold py-2 px-4  rounded focus:outline-none focus:shadow-outline"
+        class="bg-blue-300 drop-shadow-md duration-500 hover:bg-blue-400 font-semibold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
         type="button"
         on:click={handleClick}
       >
         Iniciar sesión
       </button>
       <a
-        class="my-2 bg-gray-300 drop-shadow-md duration-500 hover:bg-gray-400 font-bold py-2 px-4 text-center hover:text-black hover:no-underline rounded focus:outline-none focus:shadow-outline"
+        class="my-3 bg-gray-300 drop-shadow-md duration-500 hover:bg-gray-400 font-semibold py-2 px-4 text-center hover:text-black hover:no-underline rounded focus:outline-none focus:shadow-outline"
         href="/register"
       >
         Registrarme
