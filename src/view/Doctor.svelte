18411dca15b5c19b07d1bbf118bd416ecf4b4860
Coaxus <64023053+Coaxus-ux@users.noreply.github.com>
2022-06-14 18:40:47 -0500

    validations add

---
+++ src/view/Doctor.svelte
@@ -0,0 +1,7 @@
+<script>
+    import { onMount } from "svelte";
+    import { loggedValidator} from "../lib/loggedValidator";
+    onMount(async () => {
+      await loggedValidator();
+    });
+  </script>
---
+++ src/view/Patient.svelte
@@ -0,0 +1,10 @@
+<script>
+    import { onMount } from "svelte";
+    import { loggedValidator} from "../lib/loggedValidator";
+    onMount(async () => {
+      await loggedValidator();
+    });
+    const JWT = JSON.parse(localStorage.getItem("user"));
+</script>
+
+<h1>bienvenido doctor{ JWT.name }</h1>
